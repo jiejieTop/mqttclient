bbbf70ec5b7f6860df9fa1172f9aaef40b076731
jiejietop <jiejietop@gmail.com>
2020-01-29 20:47:55 +0800

    add tencentos tiny platform, but not test...

---
+++ platform/TencentOS-tiny/platform_memory.c
@@ -0,0 +1,22 @@
+/*
+ * @Author: jiejie
+ * @Github: https://github.com/jiejieTop
+ * @Date: 2019-12-14 22:02:07
+ * @LastEditTime : 2019-12-20 20:43:38
+ * @Description: the code belongs to jiejie, please keep the author information and source code according to the license.
+ */
+#include "tos_k.h"
+
+
+void *platform_memory_alloc(size_t size)
+{
+    return tos_mmheap_alloc(size);
+}
+
+void platform_memory_free(void *ptr)
+{
+    tos_mmheap_free(ptr);
+}
+
+
+
