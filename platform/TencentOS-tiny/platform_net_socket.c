30ed7c654e128ea0fa57fc3c379e1c57883a6894
jiejietop <jiejietop@gmail.com>
2020-03-27 01:15:46 +0800

    update tencentos platform socket and tls...

--- platform/TencentOS-tiny/platform_net_socket.c
+++ platform/TencentOS-tiny/platform_net_socket.c
@@ -2,20 +2,25 @@
  * @Author: jiejie
  * @Github: https://github.com/jiejieTop
  * @Date: 2020-01-10 23:45:59
- * @LastEditTime : 2020-01-13 02:48:53
+ * @LastEditTime: 2020-03-27 01:12:54
  * @Description: the code belongs to jiejie, please keep the author information and source code according to the license.
  */
 #include "platform_net_socket.h"
 
-#include "lwip/opt.h"
-#include "lwip/sys.h"
-#include "lwip/api.h"
-#include <lwip/sockets.h>
-#include "lwip/netdb.h"
-
 int platform_net_socket_connect(const char *host, const char *port, int proto)
 {
     int fd, ret = MQTT_SOCKET_UNKNOWN_HOST;
+#ifdef MQTT_NETSOCKET_USE_AT
+
+    fd = tos_sal_module_connect(host, port, TOS_SAL_PROTO_TCP);
+    
+    if (fd < 0) {
+        return MQTT_CONNECT_FAILED_ERROR;
+    }
+    ret = fd;
+    
+#else
+    
     struct addrinfo hints, *addr_list, *cur;
     
     /* Do name resolution with both IPv6 and IPv4 */
@@ -45,16 +50,25 @@ int platform_net_socket_connect(const char *host, const char *port, int proto)
     }
 
     freeaddrinfo(addr_list);
+#endif
+
     return ret;
 }
 
 int platform_net_socket_recv(int fd, void *buf, size_t len, int flags)
 {
+#ifdef MQTT_NETSOCKET_USE_AT
+    return tos_sal_module_recv(fd, buf, len);
+#else
     return recv(fd, buf, len, flags);
+#endif
 }
 
 int platform_net_socket_recv_timeout(int fd, unsigned char *buf, int len, int timeout)
 {
+#ifdef MQTT_NETSOCKET_USE_AT
+    return tos_sal_module_recv_timeout(fd, buf, len, timeout);
+#else
     int rc;
     int bytes = 0;
 	struct timeval tv = {
@@ -79,15 +93,23 @@ int platform_net_socket_recv_timeout(int fd, unsigned char *buf, int len, int ti
 		}
 	}
 	return bytes;
+#endif
 }
 
 int platform_net_socket_write(int fd, void *buf, size_t len)
 {
+#ifdef MQTT_NETSOCKET_USE_AT
+    return tos_sal_module_send(fd, buf, len);
+#else
     return write(fd, buf, len);
+#endif
 }
 
 int platform_net_socket_write_timeout(int fd, unsigned char *buf, int len, int timeout)
 {
+#ifdef MQTT_NETSOCKET_USE_AT
+    return tos_sal_module_send(fd, buf, len);
+#else
 	struct timeval tv = {
         timeout / 1000, 
         (timeout % 1000) * 1000
@@ -98,15 +120,22 @@ int platform_net_socket_write_timeout(int fd, unsigned char *buf, int len, int t
 		tv.tv_usec = 100;
 	}
 
-	setsockopt(fd, SOL_SOCKET, SO_SNDTIMEO, (char *)&tv,sizeof(struct timeval));
+	platform_net_socket_setsockopt(fd, SOL_SOCKET, SO_SNDTIMEO, (char *)&tv,sizeof(struct timeval));
 	
     return write(fd, buf, len);
+#endif
 }
 
 int platform_net_socket_close(int fd)
 {
+#ifdef MQTT_NETSOCKET_USE_AT
+    return tos_sal_module_close(fd);
+#else
     return close(fd);
+#endif
 }
+
+#ifndef MQTT_NETSOCKET_USE_AT
 
 int platform_net_socket_set_block(int fd)
 {
@@ -123,3 +152,4 @@ int platform_net_socket_setsockopt(int fd, int level, int optname, const void *o
     return setsockopt(fd, level, optname, optval, optlen);
 }
 
+#endif
