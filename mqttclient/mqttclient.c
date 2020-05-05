1d46bce80144b977aadd46dc9935c9e138ac85fa
EmbeddedXGJ <1741417385@qq.com>
2020-05-05 13:55:46 +0800

    code style had been modified

--- mqttclient/mqttclient.c
+++ mqttclient/mqttclient.c
@@ -936,7 +936,7 @@ static int mqtt_connect_with_results(mqtt_client_t* c)
         RETURN_ERROR(MQTT_SUCCESS_ERROR);
 
     rc = c->network->connect(c->network);
-    if (MQTT_SUCCESS_ERROR != rc){
+    if (MQTT_SUCCESS_ERROR != rc) {
         c->network->disconnect(c->network);
         RETURN_ERROR(rc);
     }  
