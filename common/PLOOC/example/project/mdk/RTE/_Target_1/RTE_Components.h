1adec3cd5d153bc4e7d7e0dd7d6844a7969a1292
Gabriel Wang <embedded_zhuoran@hotmail.com>
2020-12-01 13:59:04 +0000

    add PLOOC

---
+++ common/PLOOC/example/project/mdk/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,26 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'plooc_example' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "CMSDK_CM3.h"
+
+/*  Keil.ARM Compiler::Compiler:I/O:STDOUT:User:1.2.0 */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_User     /* Compiler I/O: STDOUT User */
+/*  Keil::CMSIS Driver:USART:1.0.3 */
+#define RTE_Drivers_USART
+
+
+#endif /* RTE_COMPONENTS_H */
