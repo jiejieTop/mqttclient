be5ba4e8bb4d1535ecfbe7add96c2e3df9863bdf
小权一句两句 <xqyjlj@126.com>
2022-08-30 10:44:24 +0800

    Update mqttclient.c

--- mqttclient/mqttclient.c
+++ mqttclient/mqttclient.c
@@ -1302,6 +1302,7 @@ int mqtt_subscribe(mqtt_client_t* c, const char* topic_filter, mqtt_qos_t qos, m
 {
     int rc = MQTT_SUBSCRIBE_ERROR;
     int len = 0;
+    int qos_level = qos; /* This can avoid alignment bugs, because enum is not int on some compilers */
     uint16_t packet_id;
     platform_timer_t timer;
     MQTTString topic = MQTTString_initializer;
@@ -1314,8 +1315,6 @@ int mqtt_subscribe(mqtt_client_t* c, const char* topic_filter, mqtt_qos_t qos, m
     platform_mutex_lock(&c->mqtt_write_lock);
 
     packet_id = mqtt_get_next_packet_id(c);
-
-    int qos_level = qos; /* This can avoid alignment bugs, because enum is not int on some compilers */
 
     /* serialize subscribe packet and send it */
     len = MQTTSerialize_subscribe(c->mqtt_write_buf, c->mqtt_write_buf_size, 0, packet_id, 1, &topic, (int *)&qos_level);
